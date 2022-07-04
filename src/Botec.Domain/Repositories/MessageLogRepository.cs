23e87c476ed427f7566ccda2b663a7ece90119aa
Yahor Anikeyenka <yahor.anikeyenka@gmail.com>
2022-07-04 14:51:15 +0300

    Add message logging

---
+++ src/Botec.Domain/Repositories/MessageLogRepository.cs
@@ -0,0 +1,19 @@
+﻿using Botec.Domain.Entities;
+
+namespace Botec.Domain.Repositories;
+
+public class MessageLogRepository
+{
+    private ApplicationDbContext _context;
+
+    public MessageLogRepository()
+    {
+        _context = new ApplicationDbContext();
+    }
+
+    public async Task LogUpdate(MessageLog messageLog, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(messageLog, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
