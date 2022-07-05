d3945f4da5f6cb428f8e389ca720b98432c98ba0
Yahor Anikeyenka <yahor.anikeyenka@gmail.com>
2022-07-05 13:00:16 +0300

    Update commands list

--- src/Botec.CommandProcessor/Commands.cs
+++ src/Botec.CommandProcessor/Commands.cs
@@ -5,36 +5,44 @@ public class Commands
     public static IEnumerable<string> RepeatCommands = new List<string> 
     { 
         "бот повтори",
+        "бот, повтори",
+        "бот скажи",
+        "бот, скажи",
         "bot repeat"
     };
 
     public static IEnumerable<string> UpdateCockCommands = new List<string>
     {
         "бот кок",
+        "бот, кок",
         "bot cock"
     };
 
     public static IEnumerable<string> CocksTopCommands = new List<string>
     {
         "бот топ коков",
+        "бот, топ коков",
         "bot cocks top"
     };
 
     public static IEnumerable<string> AbsoluteRatingCommands = new List<string>
     {
         "бот абсолют",
+        "бот, абсолют",
         "bot absolute"
     };
 
     public static IEnumerable<string> FaggotOfTheDayCommands = new List<string>
     {
         "бот пидор дня",
+        "бот, пидор дня",
         "bot faggot of the day"
     };
 
     public static IEnumerable<string> BanekJokeCommands = new List<string>
     {
         "бот анек",
+        "бот, анек",
         "bot anek"
     };
 }
